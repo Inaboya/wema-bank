8c0ad4d34285e01c171e81b6c6d76933e354c261
Inaboya <momohdavid21@gmail.com>
2024-01-01 20:34:53 +0100

    fix table

--- src/utils/tableBuilders.tsx
+++ src/utils/tableBuilders.tsx
@@ -10,6 +10,11 @@ export function buildLoanTableData(datas: any[] = []) {
 
 export const verifiersColumn = [
   {
+    header: '',
+    accessor: 'selection',
+    type: 'selection',
+  },
+  {
     header: "First Name",
     accessor: "firstName",
     type: "string",
