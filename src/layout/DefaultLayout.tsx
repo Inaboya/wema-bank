a2f00f6b41664d1ffdb1027b72b41ed0d1fbfe2e
Inaboya <momohdavid21@gmail.com>
2024-01-02 17:49:56 +0100

    fix errors

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import AdminHeader from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
